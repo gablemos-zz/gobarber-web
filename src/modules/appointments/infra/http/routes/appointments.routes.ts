a1705b1eea3f15e155c659c639c6f63842bf2c23
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2020-05-26 00:48:57 -0300

    f05

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+appointmentsRouter.post(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default appointmentsRouter;
